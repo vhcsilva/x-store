a22b5ed8f17ac298f7e4d861e42134bf9d6ebe23
vhcsilva <vhcsilvaa@gmail.com>
2024-01-12 08:45:39 -0300

    add quantity indicator

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -3,12 +3,21 @@ import {Icon} from "@taikai/rocket-kit";
 import styles from "@/components/CartButton/styles.module.css";
 import {useState} from "react";
 import CartDrawer from "@/components/CartDrawer";
+import {useAppContext} from "@/stores/app-store";
 
 export default function CartButton () {
   const [isShowing, setIsShowing] = useState(false);
 
+  const { cart } = useAppContext();
+
+  const cartQuantity = cart.length;
+
   return (
-    <>
+    <div className={styles.wrapper}>
+      <div className={styles.quantity}>
+        {cartQuantity}
+      </div>
+
       <button
         className={styles.cartButton}
         onClick={() => setIsShowing(true)}
@@ -24,6 +33,6 @@ export default function CartButton () {
         isShowing={isShowing}
         onHide={() => setIsShowing(false)}
       />
-    </>
+    </div>
   );
 }
